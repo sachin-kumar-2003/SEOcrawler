f7a27a1f973c69c5c84282901d2fa1b9ceb0c31e
sachin kumar <goputen215@gmail.com>
2025-07-25 19:39:23 +0530

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
 import json
 import aiofiles
-from .crawler import bfs
+from crawler import bfs
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import asyncio
